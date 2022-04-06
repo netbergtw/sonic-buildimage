22736988d58549af5b452eca639177a0a396ad03
Andrew Sapronov <andrew.sapronov@gmail.com>
2022-04-06 18:08:27 +0300

    [nba610] Fixed platform name

--- platform/barefoot/sonic-platform-modules-netberg/aurora-610/sonic_platform/qsfp.py
+++ platform/barefoot/sonic-platform-modules-netberg/aurora-610/sonic_platform/qsfp.py
@@ -70,7 +70,7 @@ QSFP_REG_VALUE_DISABLE = "0x0"
 
 class QSfp(SfpBase):
 
-    __platform = "x86_64-netberg-aurora-610-r0"
+    __platform = "x86_64-netberg_aurora_610-r0"
     __hwsku = "aurora-610"
     __port_to_i2c_mapping = {
         0: 10,  1: 11,  2: 12,  3: 13,  4: 14,  5: 15,  6: 16,  7: 17,
